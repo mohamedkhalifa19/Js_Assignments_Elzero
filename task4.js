2b5337b1221069db96f5acbf4c6bb42d48cdce87
Mohamed Khalifa <91298305+mohamedkhalifa19@users.noreply.github.com>
2023-02-27 01:41:50 +0200

    Create task4.js

---
+++ task4.js
@@ -0,0 +1,6 @@
+console.group("%cProgramming Lang::","color:#ffff;background:red;padding:30px;font-weight:bold;font-size:35px");
+console.group("%cHTML","background:yellow;padding:20px;border-radius:50%");
+   console.groupEnd();
+console.group("%cCSS","background:blue;padding:20px;border-radius:50%;color:#ffff;font-weight:bold");
+  console.groupEnd();
+console.groupEnd();
